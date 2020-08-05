2598c4ed789b73da97ee9983483ade01c2969e74
Ruchir Dixit <ruchirtd96@gmail.com>
2020-08-05 16:59:23 +0530

    Check random attendance

--- EmpWageComputationProg.java
+++ EmpWageComputationProg.java
@@ -2,6 +2,12 @@ public class EmpWageComputationProg
 {
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to Employee Wage Computation Program");
+		//System.out.println("Welcome to Employee Wage Computation Program");
+		final int IS_PRESENT=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_PRESENT)
+			System.out.println("Emp is present");
+		else
+			System.out.println("Emp is absent");
 	}
 }
